baafc85980f84e69332462d0235f4d6aed636884
shan2 <shan2@2f3b4d84-7048-4793-a15c-83de0941b73a>
2012-07-04 14:35:59 +0000

    ForSyDe-SystemC: Added a toy example for the new DE MoC.

---
+++ ForSyDe-SystemC/trunk/examples/de/toyde/report.hpp
@@ -0,0 +1,30 @@
+/**********************************************************************
+    * report.hpp -- the report process                                *
+    *                                                                 *
+    * Author:  Hosien Attarzadeh (shan2@kth.se)                       *
+    *                                                                 *
+    * Purpose: Demonstration of a simple DE system.                   *
+    *                                                                 *
+    * Usage:   ToyDE example                                          *
+    *                                                                 *
+    * License: BSD3                                                   *
+    *******************************************************************/
+
+
+#ifndef REPORT_HPP
+#define REPORT_HPP
+
+#include <forsyde.hpp>
+#include <iostream>
+
+using namespace ForSyDe;
+using namespace ForSyDe::DE;
+
+void report_func(tt_event<int> inp1)
+{
+#pragma ForSyDe begin report_func
+    std::cout << "output value: " << inp1 << std::endl;
+#pragma ForSyDe end
+}
+
+#endif
