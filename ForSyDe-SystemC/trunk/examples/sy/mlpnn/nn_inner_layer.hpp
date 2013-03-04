2bc345b5d317b228950f78c1f07d4d5d52cb867b
shan2 <shan2@2f3b4d84-7048-4793-a15c-83de0941b73a>
2013-03-04 07:21:59 +0000

    ForSyDe-SystemC: added the MLP Neural Network example which performs vector operations

---
+++ ForSyDe-SystemC/trunk/examples/sy/mlpnn/nn_inner_layer.hpp
@@ -0,0 +1,34 @@
+/**********************************************************************
+    * add.hpp -- an adder process                                     *
+    *                                                                 *
+    * Author:  Hosien Attarzadeh (shan2@kth.se)                       *
+    *                                                                 *
+    * Purpose: Demonstration of a simple sequential process.          *
+    *                                                                 *
+    * Usage:   MulAcc example                                         *
+    *                                                                 *
+    * License: BSD3                                                   *
+    *******************************************************************/
+
+
+#ifndef NN_INNER_LAYER_HPP
+#define NN_INNER_LAYER_HPP
+
+#include <forsyde.hpp>
+
+using namespace ForSyDe;
+using namespace ForSyDe::SY;
+
+void nn_inner_layer_func(abst_ext<float>& out1,
+			const std::array<abst_ext<float>,NN_NUM_INPUTS>& inp)
+{
+#pragma ForSyDe begin nn_inner_layer_func
+	int i;
+	int res = 0;
+	for (i=0;i<NN_NUM_INPUTS;i++)
+		res += from_abst_ext(inp[i],0);
+	set_val(out1,res);
+#pragma ForSyDe end
+}
+
+#endif
