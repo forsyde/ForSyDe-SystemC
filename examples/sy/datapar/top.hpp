a0ca8220b8d4ae208093fd3a909bf8cc18b7744a
shan2 <shaniaki@users.noreply.github.com>
2014-04-01 14:45:48 +0000

    ForSyDe-SystemC: Enhance data-parallel constructors and add OpenMP-based simulation speedup.

--- examples/sy/datapar/top.hpp
+++ examples/sy/datapar/top.hpp
@@ -19,20 +19,22 @@
 using namespace ForSyDe;
 using namespace std;
 
-std::array<int,10> inpval = {{0,1,2,3,4,5,6,7,8,9}};
+//std::array<int,10> inpval = {{0,1,2,3,4,5,6,7,8,9}};
+std::array<int,10000> inpval;
 
 SC_MODULE(top)
 {
-    SY::signal<std::array<int,10>> srca, srcb;
+    SY::signal<std::array<int,10000>> srca, srcb;
     SY::signal<int> result;
     
     SC_CTOR(top)
     {
+        inpval.fill(1);
         SY::make_sconstant("constant1", inpval, 10, srca);
         
-        SY::make_sdpcomb("inc1", inc_func, srcb, srca);
+        SY::make_sdpmap("inc1", inc_func, srcb, srca);
         
-        SY::make_sreduce("add1", add_func, result, srcb);
+        SY::make_sdpreduce("add1", add_func, result, srcb);
         
         SY::make_ssink("report1", report_func, result);
     }
