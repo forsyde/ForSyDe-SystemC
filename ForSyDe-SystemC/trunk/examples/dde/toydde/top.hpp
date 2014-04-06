6d5e01f336a54271e62040067d1665f67c3e164b
shan2 <shan2@2f3b4d84-7048-4793-a15c-83de0941b73a>
2014-04-06 09:22:44 +0000

    ForSyDe-SystemC: ported the toy example from the DE MoC to the new DDE version,

--- ForSyDe-SystemC/trunk/examples/dde/toydde/top.hpp
+++ ForSyDe-SystemC/trunk/examples/dde/toydde/top.hpp
@@ -3,9 +3,9 @@
     *                                                                 *
     * Author:  Hosien Attarzadeh (shan2@kth.se)                       *
     *                                                                 *
-    * Purpose: Demonstration of a simple DE system.                   *
+    * Purpose: Demonstration of a simple DDE system.                  *
     *                                                                 *
-    * Usage:   ToyDE example                                          *
+    * Usage:   ToyDDE example                                         *
     *                                                                 *
     * License: BSD3                                                   *
     *******************************************************************/
@@ -19,20 +19,24 @@ using namespace ForSyDe;
 
 SC_MODULE(top)
 {
-    DE::signal<int> srca, feedback, addi1, addi2, result;
+    DDE::signal<int> srca, feedback, addi1, addi2, result;
     
     SC_CTOR(top)
     {
-        DE::make_delay("delay1", 0, sc_time(10, SC_NS), srca, feedback);
+        DDE::make_delay("delay1", 0, sc_time(10, SC_NS), srca, feedback);
         
-        auto inc1 = DE::make_comb("inc1", inc_func, addi1, srca);
+        auto inc1 = DDE::make_comb("inc1", inc_func, addi1, srca);
         inc1->oport1(feedback);
         
-        DE::make_constant("const1", -1, sc_time(100,SC_NS), addi2);
+        DDE::make_vsource("const1",
+                std::vector<int>(1,7),
+                std::vector<sc_time>(1,sc_time(100,SC_NS)),
+                addi2
+        );
         
-        DE::make_comb2("add1", add_func, result, addi1, addi2);
+        DDE::make_comb2("add1", add_func, result, addi1, addi2);
         
-        DE::make_sink("report1", report_func, result);
+        DDE::make_sink("report1", report_func, result);
     }
 #ifdef FORSYDE_INTROSPECTION
     void start_of_simulation()
