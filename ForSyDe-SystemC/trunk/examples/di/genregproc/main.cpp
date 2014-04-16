5c59a63126ffbd74c39e77ed97423d31e87f4cb0
shan2 <shan2@2f3b4d84-7048-4793-a15c-83de0941b73a>
2014-04-16 21:52:55 +0000

    ForSyDe-SystemC: add a heterogeneous generator/regulator/protector example

---
+++ ForSyDe-SystemC/trunk/examples/di/genregproc/main.cpp
@@ -0,0 +1,25 @@
+/**********************************************************************
+    * main.cpp -- the top module and testbench for the Generator/     *
+    *          Regulator/Protector example                            *
+    *                                                                 *
+    * Author:  Hosein Attarzadeh (shan2@kth.se)                       *
+    *          Based on the example from chapter 1 of:                *
+    *          System Design, Modeling, and Simulation using PtolemyII*
+    *                                                                 *
+    * Purpose: Demonstration of a single cyber-physical system        *
+    *                                                                 *
+    * Usage:   Generator/Regulator/Protector example                  *
+    *                                                                 *
+    * License: BSD3                                                   *
+    *******************************************************************/
+
+#include "top.hpp"
+
+int sc_main(int argc, char **argv)
+{
+    top top("top");
+
+    sc_start();
+        
+    return 0;
+}
