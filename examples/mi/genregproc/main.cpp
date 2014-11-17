19617a9467b98543ead07e488fae2ec9f8c085a4
shan2 <shaniaki@users.noreply.github.com>
2014-11-17 15:04:32 +0000

    ForSyDe-SystemC: fixes and improvements to the GenRegProc example.

--- examples/mi/genregproc/main.cpp
+++ examples/mi/genregproc/main.cpp
@@ -14,12 +14,15 @@
     *******************************************************************/
 
 #include "top.hpp"
+//~ #include "gentest.hpp"
+
 
 int sc_main(int argc, char **argv)
 {
     top top("top");
 
     sc_start(60,SC_SEC);
-        
+    
+    sc_stop();    
     return 0;
 }
