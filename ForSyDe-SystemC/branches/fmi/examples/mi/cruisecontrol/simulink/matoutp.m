d56e730616727db28c5d7e144e8a2235c069ecfe
shan2 <shan2@2f3b4d84-7048-4793-a15c-83de0941b73a>
2015-06-26 15:15:32 +0000

    Remove the fmi branch, reintegrated with trunk in r322.

--- ForSyDe-SystemC/branches/fmi/examples/mi/cruisecontrol/simulink/matoutp.m
+++
@@ -1,9 +0,0 @@
-function out = matoutp( inp )
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-
-fprintf(evalin('base','fidout'),'%f\n',inp);
-%fclose(evalin('base','fidout'));
-%evalin('base','fidout = fopen(''forsyde_out'',''w'');');
-out=0;
-end
