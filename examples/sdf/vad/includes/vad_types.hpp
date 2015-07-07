e3d0e2b4dcef3bd913d02b3cb798ae95d9299590
Hosein Attarzadeh <shan2@kth.se>
2015-07-07 17:49:37 +0200

    enrich type introspection information

--- examples/sdf/vad/includes/vad_types.hpp
+++ examples/sdf/vad/includes/vad_types.hpp
@@ -35,4 +35,22 @@ std::ostream& operator <<(std::ostream &os,const Pfloat &obj)
       return os;
 }
 
+DEFINE_TYPE_NAME(r_t,"r_t");
+DEFINE_TYPE_NAME(rc_t,"rc_t");
+DEFINE_TYPE_NAME(rav1_t,"rav1_t");
+DEFINE_TYPE_NAME(Pfloat,"Pfloat");
+DEFINE_TYPE_NAME(pvad_acf0_t,"pvad_acf0_t");
+
+//~ DEFINE_TYPE_NAME(tuple_of_vectors<L_av_t,L_av_t>,"r_t.r_t");
+template<>const char* get_type_name<tuple_of_vectors<L_av_t,L_av_t>>(){return "r_t.r_t";}
+//~ DEFINE_TYPE_NAME(tuple_of_vectors<rav1_t,short,pvad_acf0_t,short,short>,"rav1_t.short.pvad_acf0_t.short.short");
+template<>const char* get_type_name<tuple_of_vectors<rav1_t,short,pvad_acf0_t,short,short>>(){return "rav1_t.short.pvad_acf0_t.short.short";}
+//~ DEFINE_TYPE_NAME(tuple_of_vectors<pvad_acf0_t,Pfloat>,"pvad_acf0_t.Pfloat");
+template<>const char* get_type_name<tuple_of_vectors<pvad_acf0_t,Pfloat>>(){return "pvad_acf0_t.Pfloat";}
+//~ DEFINE_TYPE_NAME(tuple_of_vectors<rvad_t,Pfloat>,"rav1_t.Pfloat");
+template<>const char* get_type_name<tuple_of_vectors<rvad_t,Pfloat>>(){return "rav1_t.Pfloat";}
+//~ DEFINE_TYPE_NAME(tuple_of_vectors<r_t,r_t,r_t,short,short,rc_t,short>,"r_t.r_t.r_t.short.short.rc_t.short");
+template<>const char* get_type_name<tuple_of_vectors<r_t,r_t,r_t,short,short,rc_t,short>>(){return "r_t.r_t.r_t.short.short.rc_t.short";}
+
+
 #endif
