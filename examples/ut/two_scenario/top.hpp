0c8c9d79489f494d047026149e15e8a7187b6ce6
Mohammad Vazirpanah <60341840+mohammadvazirpanah@users.noreply.github.com>
2022-07-26 21:38:48 +0430

    Approved comments

--- examples/ut/two_scenario/top.hpp
+++ examples/ut/two_scenario/top.hpp
@@ -10,9 +10,9 @@ using namespace ForSyDe;
 SC_MODULE(top)
 {
     UT::signal<int> from_source, from_constant, from_kernels;
-    UT::signal<unsigned int> zip_control;
-    UT::signal<std::tuple<std::vector<int>,std::vector<std::tuple<unsigned int, unsigned int, scenrio_func>>>> from_zip;
-    UT::signal<std::tuple<unsigned int, unsigned int, scenrio_func>> from_detector, from_detector2; 
+    UT::signal<size_t> zip_control;
+    UT::signal<std::tuple<std::vector<int>,std::vector<std::tuple<size_t, size_t, scenrio_func>>>> from_zip;
+    UT::signal<std::tuple<size_t, size_t, scenrio_func>> from_detector, from_detector2; 
 
     SC_CTOR(top)
     {        
@@ -31,21 +31,21 @@ SC_MODULE(top)
 
         detector->oport1(from_detector2);
                         
-        UT::make_comb("cextract",[](std::vector<unsigned int>& out,const std::vector<std::tuple<unsigned int, unsigned int, scenrio_func>>& inp){
+        UT::make_comb("cextract",[](std::vector<size_t>& out,const std::vector<std::tuple<size_t, size_t, scenrio_func>>& inp){
             out.resize(1);
             out[0] = std::get<0>(inp[0]);
         }, 1, zip_control, from_detector2);
         
-        /// -> without helper 
+        /// -> Without using helper 
         
-        /* auto zipU1 = new UT::zipU <int, std::tuple<unsigned int, unsigned int, scenrio_func>, unsigned int>("zipU1", gamma_func_zipa, gamma_func_zipb);
+        // auto zipU1 = new UT::zipU <int, std::tuple<size_t, size_t, scenrio_func>, size_t>("zipU1", gamma_func_zipa, gamma_func_zipb);
         // zipU1->iport1 (from_source);   
         // zipU1->iport2 (from_detector);
         // zipU1->controlport (zip_control);
-         zipU1->oport1 (from_zip); 
-         */ 
+        // zipU1->oport1 (from_zip); 
+         
         
-        // -> Using helper 
+        // -> using helper 
         
         UT::make_zipU("zipU1", gamma_func_zipa, gamma_func_zipb, from_zip, from_source, from_detector, zip_control);
         UT::make_comb ("kernels",
@@ -56,7 +56,6 @@ SC_MODULE(top)
                         );
 
         UT::make_sink("sink", report_func, from_kernels);
-    
     }
 
 };
